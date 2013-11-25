f79688e21de50025e304c5d15dc9f15f2ed5fb30
Scott Blevins <sblevins@gmail.com>
2013-11-25 13:09:39 -0600

    Conforming to PEP8 Guidelines
    EG. Use 4 spaces per indentation level.
    
    http://www.python.org/dev/peps/pep-0008
    
    All Python files checked with the pep8 tool:
    https://pypi.python.org/pypi/pep8

--- pydrive/apiattr.py
+++ pydrive/apiattr.py
@@ -1,173 +1,174 @@
 class ApiAttribute(object):
-  """A data descriptor that sets and returns values."""
+    """A data descriptor that sets and returns values."""
 
-  def __init__(self, name):
-    """Create an instance of ApiAttribute.
+    def __init__(self, name):
+        """Create an instance of ApiAttribute.
 
-    :param name: name of this attribute.
-    :type name: str.
-    """
-    self.name = name
+        :param name: name of this attribute.
+        :type name: str.
+        """
+        self.name = name
 
-  def __get__(self, obj, type=None):
-    """Accesses value of this attribute."""
-    return obj.attr.get(self.name)
+    def __get__(self, obj, type=None):
+        """Accesses value of this attribute."""
+        return obj.attr.get(self.name)
 
-  def __set__(self, obj, value):
-    """Write value of this attribute."""
-    obj.attr[self.name] = value
-    if obj.dirty.get(self.name) is not None:
-      obj.dirty[self.name] = True
+    def __set__(self, obj, value):
+        """Write value of this attribute."""
+        obj.attr[self.name] = value
+        if obj.dirty.get(self.name) is not None:
+            obj.dirty[self.name] = True
 
-  def __del__(self, obj):
-    """Delete value of this attribute."""
-    del obj.attr[self.name]
-    if obj.dirty.get(self.name) is not None:
-      del obj.dirty[self.name]
+    def __del__(self, obj):
+        """Delete value of this attribute."""
+        del obj.attr[self.name]
+        if obj.dirty.get(self.name) is not None:
+            del obj.dirty[self.name]
 
 
 class ApiAttributeMixin(object):
-  """Mixin to initialize required global variables to use ApiAttribute."""
+    """Mixin to initialize required global variables to use ApiAttribute."""
 
-  def __init__(self):
-    self.attr = {}
-    self.dirty = {}
+    def __init__(self):
+        self.attr = {}
+        self.dirty = {}
 
 
 class ApiResource(dict):
-  """Super class of all api resources.
+    """Super class of all api resources.
 
-  Inherits and behaves as a python dictionary to handle api resources.
-  Save clean copy of metadata in self.metadata as a dictionary.
-  Provides changed metadata elements to efficiently update api resources.
-  """
-  auth = ApiAttribute('auth')
-
-  def __init__(self, *args, **kwargs):
-    """Create an instance of ApiResource."""
-    self.update(*args, **kwargs)
-
-  def __getitem__(self, key):
-    """Overwritten method of dictionary.
-
-    :param key: key of the query.
-    :type key: str.
-    :returns: value of the query.
+    Inherits and behaves as a python dictionary to handle api resources.
+    Save clean copy of metadata in self.metadata as a dictionary.
+    Provides changed metadata elements to efficiently update api resources.
     """
-    return dict.__getitem__(self, key)
+    auth = ApiAttribute('auth')
 
-  def __setitem__(self, key, val):
-    """Overwritten method of dictionary.
+    def __init__(self, *args, **kwargs):
+        """Create an instance of ApiResource."""
+        self.update(*args, **kwargs)
 
-    :param key: key of the query.
-    :type key: str.
-    :param val: value of the query.
-    """
-    dict.__setitem__(self, key, val)
+    def __getitem__(self, key):
+        """Overwritten method of dictionary.
 
-  def __repr__(self):
-    """Overwritten method of dictionary."""
-    dictrepr = dict.__repr__(self)
-    return '%s(%s)' % (type(self).__name__, dictrepr)
+        :param key: key of the query.
+        :type key: str.
+        :returns: value of the query.
+        """
+        return dict.__getitem__(self, key)
 
-  def update(self, *args, **kwargs):
-    """Overwritten method of dictionary."""
-    for k, v in dict(*args, **kwargs).iteritems():
-      self[k] = v
+    def __setitem__(self, key, val):
+        """Overwritten method of dictionary.
 
-  def UpdateMetadata(self, metadata=None):
-    """Update metadata and mark all of them to be clean."""
-    if metadata:
-      self.update(metadata)
-    self.metadata = dict(self)
+        :param key: key of the query.
+        :type key: str.
+        :param val: value of the query.
+        """
+        dict.__setitem__(self, key, val)
 
-  def GetChanges(self):
-    """Returns changed metadata elements to update api resources efficiently.
+    def __repr__(self):
+        """Overwritten method of dictionary."""
+        dictrepr = dict.__repr__(self)
+        return '%s(%s)' % (type(self).__name__, dictrepr)
 
-    :returns: dict -- changed metadata elements.
-    """
-    dirty = {}
-    for key in self:
-      if self.metadata.get(key) is None:
-        dirty[key] = self[key]
-      elif self.metadata[key] != self[key]:
-        dirty[key] = self[key]
-    return dirty
+    def update(self, *args, **kwargs):
+        """Overwritten method of dictionary."""
+        for k, v in dict(*args, **kwargs).iteritems():
+            self[k] = v
+
+    def UpdateMetadata(self, metadata=None):
+        """Update metadata and mark all of them to be clean."""
+        if metadata:
+            self.update(metadata)
+        self.metadata = dict(self)
+
+    def GetChanges(self):
+        """Returns changed metadata elements to update api resources
+        efficiently.
+
+        :returns: dict -- changed metadata elements.
+        """
+        dirty = {}
+        for key in self:
+            if self.metadata.get(key) is None:
+                dirty[key] = self[key]
+            elif self.metadata[key] != self[key]:
+                dirty[key] = self[key]
+        return dirty
 
 
 class ApiResourceList(ApiAttributeMixin, ApiResource):
-  """Abstract class of all api list resources.
+    """Abstract class of all api list resources.
 
-  Inherits ApiResource and builds iterator to list any API resource.
-  """
-  metadata = ApiAttribute('metadata')
-
-  def __init__(self, auth=None, metadata=None):
-    """Create an instance of ApiResourceList.
-
-    :param auth: authorized GoogleAuth instance.
-    :type auth: GoogleAuth.
-    :param metadata: parameter to send to list command.
-    :type metadata: dict.
+    Inherits ApiResource and builds iterator to list any API resource.
     """
-    ApiAttributeMixin.__init__(self)
-    ApiResource.__init__(self)
-    self.auth = auth
-    self.UpdateMetadata()
-    if metadata:
-      self.update(metadata)
+    metadata = ApiAttribute('metadata')
 
-  def __iter__(self):
-    """Returns iterator object.
+    def __init__(self, auth=None, metadata=None):
+        """Create an instance of ApiResourceList.
 
-    :returns: ApiResourceList -- self
-    """
-    return self
+        :param auth: authorized GoogleAuth instance.
+        :type auth: GoogleAuth.
+        :param metadata: parameter to send to list command.
+        :type metadata: dict.
+        """
+        ApiAttributeMixin.__init__(self)
+        ApiResource.__init__(self)
+        self.auth = auth
+        self.UpdateMetadata()
+        if metadata:
+            self.update(metadata)
 
-  def next(self):
-    """Make API call to list resources and return them.
+    def __iter__(self):
+        """Returns iterator object.
 
-    Auto updates 'pageToken' everytime it makes API call and
-    raises StopIteration when it reached the end of iteration.
+        :returns: ApiResourceList -- self
+        """
+        return self
 
-    :returns: list -- list of API resources.
-    :raises: StopIteration
-    """
-    if 'pageToken' in self and self['pageToken'] is None:
-      raise StopIteration
-    result = self._GetList()
-    self['pageToken'] = self.metadata.get('nextPageToken')
-    return result
+    def next(self):
+        """Make API call to list resources and return them.
 
-  def GetList(self):
-    """Get list of API resources.
+        Auto updates 'pageToken' everytime it makes API call and
+        raises StopIteration when it reached the end of iteration.
 
-    If 'maxResults' is not specified, it will automatically iterate through
-    every resources available. Otherwise, it will make API call once and
-    update 'pageToken'.
+        :returns: list -- list of API resources.
+        :raises: StopIteration
+        """
+        if 'pageToken' in self and self['pageToken'] is None:
+            raise StopIteration
+        result = self._GetList()
+        self['pageToken'] = self.metadata.get('nextPageToken')
+        return result
 
-    :returns: list -- list of API resources.
-    """
-    if self.get('maxResults') is None:
-      self['maxResults'] = 1000
-      result = []
-      for x in self:
-        result.extend(x)
-      del self['maxResults']
-      return result
-    else:
-      return self.next()
+    def GetList(self):
+        """Get list of API resources.
 
-  def _GetList(self):
-    """Helper function which actually makes API call.
+        If 'maxResults' is not specified, it will automatically iterate through
+        every resources available. Otherwise, it will make API call once and
+        update 'pageToken'.
 
-    Should be overwritten.
+        :returns: list -- list of API resources.
+        """
+        if self.get('maxResults') is None:
+            self['maxResults'] = 1000
+            result = []
+            for x in self:
+                result.extend(x)
+            del self['maxResults']
+            return result
+        else:
+            return self.next()
 
-    :raises: NotImplementedError
-    """
-    raise NotImplementedError
+    def _GetList(self):
+        """Helper function which actually makes API call.
 
-  def Reset(self):
-    """Resets current iteration"""
-    if 'pageToken' in self:
-      del self['pageToken']
+        Should be overwritten.
+
+        :raises: NotImplementedError
+        """
+        raise NotImplementedError
+
+    def Reset(self):
+        """Resets current iteration"""
+        if 'pageToken' in self:
+            del self['pageToken']
--- pydrive/test/test_oauth.py
+++ pydrive/test/test_oauth.py
@@ -5,63 +5,64 @@ import time
 
 from pydrive.auth import GoogleAuth
 
+
 class GoogleAuthTest(unittest.TestCase):
-  """Tests basic OAuth2 operations of auth.GoogleAuth."""
+    """Tests basic OAuth2 operations of auth.GoogleAuth."""
 
-  def test_01_LocalWebserverAuthWithClientConfigFromFile(self):
-    # Delete old credentials file
-    self.DeleteOldCredentialsFile('credentials/1.dat')
-    # Test if authentication works with config read from file
-    ga = GoogleAuth('settings/test1.yaml')
-    ga.LocalWebserverAuth()
-    self.assertEqual(ga.access_token_expired, False)
-    # Test if correct credentials file is created
-    self.CheckCredentialsFile('credentials/1.dat')
-    time.sleep(1)
+    def test_01_LocalWebserverAuthWithClientConfigFromFile(self):
+        # Delete old credentials file
+        self.DeleteOldCredentialsFile('credentials/1.dat')
+        # Test if authentication works with config read from file
+        ga = GoogleAuth('settings/test1.yaml')
+        ga.LocalWebserverAuth()
+        self.assertEqual(ga.access_token_expired, False)
+        # Test if correct credentials file is created
+        self.CheckCredentialsFile('credentials/1.dat')
+        time.sleep(1)
 
-  def test_02_LocalWebserverAuthWithClientConfigFromSettings(self):
-    # Delete old credentials file
-    self.DeleteOldCredentialsFile('credentials/2.dat')
-    # Test if authentication works with config read from settings
-    ga = GoogleAuth('settings/test2.yaml')
-    ga.LocalWebserverAuth()
-    self.assertEqual(ga.access_token_expired, False)
-    # Test if correct credentials file is created
-    self.CheckCredentialsFile('credentials/2.dat')
-    time.sleep(1)
+    def test_02_LocalWebserverAuthWithClientConfigFromSettings(self):
+        # Delete old credentials file
+        self.DeleteOldCredentialsFile('credentials/2.dat')
+        # Test if authentication works with config read from settings
+        ga = GoogleAuth('settings/test2.yaml')
+        ga.LocalWebserverAuth()
+        self.assertEqual(ga.access_token_expired, False)
+        # Test if correct credentials file is created
+        self.CheckCredentialsFile('credentials/2.dat')
+        time.sleep(1)
 
-  def test_03_LocalWebServerAuthWithNoCredentialsSaving(self):
-    # Delete old credentials file
-    self.DeleteOldCredentialsFile('credentials/4.dat')
-    # Provide wrong credentials file
-    ga = GoogleAuth('settings/test3.yaml')
-    ga.LocalWebserverAuth()
-    self.assertEqual(ga.access_token_expired, False)
-    # Test if correct credentials file is created
-    self.CheckCredentialsFile('credentials/4.dat', no_file=True)
-    time.sleep(1)
+    def test_03_LocalWebServerAuthWithNoCredentialsSaving(self):
+        # Delete old credentials file
+        self.DeleteOldCredentialsFile('credentials/4.dat')
+        # Provide wrong credentials file
+        ga = GoogleAuth('settings/test3.yaml')
+        ga.LocalWebserverAuth()
+        self.assertEqual(ga.access_token_expired, False)
+        # Test if correct credentials file is created
+        self.CheckCredentialsFile('credentials/4.dat', no_file=True)
+        time.sleep(1)
 
-  def test_04_CommandLineAuthWithClientConfigFromFile(self):
-    # Delete old credentials file
-    self.DeleteOldCredentialsFile('credentials/1.dat')
-    # Test if authentication works with config read from file
-    ga = GoogleAuth('settings/test4.yaml')
-    ga.CommandLineAuth()
-    self.assertEqual(ga.access_token_expired, False)
-    # Test if correct credentials file is created
-    self.CheckCredentialsFile('credentials/1.dat')
-    time.sleep(1)
+    def test_04_CommandLineAuthWithClientConfigFromFile(self):
+        # Delete old credentials file
+        self.DeleteOldCredentialsFile('credentials/1.dat')
+        # Test if authentication works with config read from file
+        ga = GoogleAuth('settings/test4.yaml')
+        ga.CommandLineAuth()
+        self.assertEqual(ga.access_token_expired, False)
+        # Test if correct credentials file is created
+        self.CheckCredentialsFile('credentials/1.dat')
+        time.sleep(1)
 
-  def DeleteOldCredentialsFile(self, credentials):
-    try:
-      os.remove(credentials)
-    except OSError:
-      pass
+    def DeleteOldCredentialsFile(self, credentials):
+        try:
+            os.remove(credentials)
+        except OSError:
+            pass
 
-  def CheckCredentialsFile(self, credentials, no_file=False):
-    ga = GoogleAuth('settings/default.yaml')
-    ga.LoadCredentialsFile(credentials)
-    self.assertEqual(ga.access_token_expired, no_file)
+    def CheckCredentialsFile(self, credentials, no_file=False):
+        ga = GoogleAuth('settings/default.yaml')
+        ga.LoadCredentialsFile(credentials)
+        self.assertEqual(ga.access_token_expired, no_file)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
