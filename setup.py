e595e3c23a6e87c950e9f42840bd652151e06b26
Ruslan Kuprieiev <kupruser@gmail.com>
2020-01-28 01:27:57 +0200

    pydrive2: bump to 1.4.2
    1.4.1 was released by me earlier.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyDrive2',
-    version='1.4.0',
+    version='1.4.2',
     author='JunYoung Gwak',
     author_email='jgwak@dreamylab.com',
     maintainer='DVC team',
